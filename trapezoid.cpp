8ea498dde67b91de338b9194a39e95f8e0e697e8
NotEmily3203 <linemily62@gmail.com>
2023-02-23 12:18:47 -0500

    added impossible shape

--- trapezoid.cpp
+++ trapezoid.cpp
@@ -3,16 +3,20 @@
 std::string trapezoid(int width, int height){
     std::string result="";
     int out = 0;
-    for(int i = height; i>0; i--){
-        for(int j=0; j<out; j++){
-            result+=" ";
+    if(width-height<=height || width<=0 || height<=0){
+        result="Impossible shape!";
+    }else{
+        for(int i = height; i>0; i--){
+            for(int j=0; j<out; j++){
+                result+=" ";
+            }
+            for(int k=0; k<width; k++){
+                result+="*";
+            }
+            out+=1;
+            width-=2;
+            result+="\n";
         }
-        for(int k=0; k<width; k++){
-            result+="*";
-        }
-        out+=1;
-        width-=2;
-        result+="\n";
     }
     return result;
 }
